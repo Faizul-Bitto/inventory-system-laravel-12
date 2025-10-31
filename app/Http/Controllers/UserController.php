ded78f9fc99dba7a024642b282e36010ba46701e
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-10-31 16:30:30 +0600

    Add user registration functionality in UserController, including validation and error handling. Update API and web routes to support user registration endpoint.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+    public function userRegistration( Request $request ) {
+
+        try {
+            $request->validate( [
+                'name'     => 'required',
+                'mobile'   => 'required',
+                'email'    => 'required',
+                'password' => 'required',
+            ] );
+
+            $name     = $request->input( "name" );
+            $mobile   = $request->input( "mobile" );
+            $email    = $request->input( "email" );
+            $password = $request->input( "password" );
+
+            $user = User::create( [
+                'name'     => $name,
+                'mobile'   => $mobile,
+                'email'    => $email,
+                'password' => Hash::make( $password ),
+            ] );
+
+            return response()->json( [
+                'status'  => 'success',
+                'message' => 'User registered successfully',
+                'user'    => $user,
+            ], 201 );
+
+        } catch ( Exception $e ) {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => $e->getMessage(),
+            ], 500 );
+        }
+
+    }
+
 }
