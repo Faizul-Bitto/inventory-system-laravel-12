ba203c6eff3022e5e2cfe4f0e9b859df2831be29
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-10-31 16:46:07 +0600

    Add user login functionality in UserController, including validation and JWT token generation. Update API and web routes to support user login endpoint.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Exception;
 use App\Models\User;
+use App\Helpers\JwtToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -41,6 +42,36 @@ class UserController extends Controller {
                 'status'  => 'failed',
                 'message' => $e->getMessage(),
             ], 500 );
+        }
+
+    }
+
+    public function userLogin( Request $request ) {
+
+        $request->validate( [
+            'email'    => 'required|email',
+            'password' => 'required',
+        ] );
+
+        $email    = $request->input( 'email' );
+        $password = $request->input( 'password' );
+
+        $user = User::where( 'email', '=', $email )->first();
+
+        if ( $user && Hash::check( $password, $user->password ) ) {
+            $token = JwtToken::createToken( $request->input( 'email' ), $user->id );
+
+            return response()->json( [
+                'status'  => 'success',
+                'message' => 'User logged in successfully',
+                'user'    => $user,
+                'token'   => $token,
+            ], 200 )->cookie( 'token', $token, time() + 60 * 24 * 30 );
+        } else {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => 'User not found',
+            ], 404 );
         }
 
     }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::post( "/user-registration", [UserController::class, "userRegistration"] )->name( "userRegistration" );
+Route::post( "/user-login", [UserController::class, "userLogin"] )->name( "userLogin" );
